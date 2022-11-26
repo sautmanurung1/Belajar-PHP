66818fbac129b08eb192294177d4b574f4364c6c
sautmanurung1 <smanurung360@gmail.com>
2022-11-27 01:44:20 +0700

    update error code

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if(! isset($_SESSION['is_login']))
               <hr class="dropdown-divider">
             </li>
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <a class="dropdown-item d-flex align-items-center" href="#">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
               </a>
